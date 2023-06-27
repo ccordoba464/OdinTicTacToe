d7fb52c2d29b272f46d3aaa8d18a662cb23c7485
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-06-27 16:46:06 -0400

    Declare choosePane function and fixed displayChoices function

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const gameBoardModule = (() => {
-  const gameBoard = ["X", "X", "O", "X", "X", "X", "X", "X", "X"];
-  return { gameBoard };
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+  const choosePane = paneID => {
+    console.log(paneID);
+    gameBoard[+paneID.charAt(5) - 1] = "O";
+    displayController.displayChoices(gameBoard);
+  };
+
+  return { gameBoard, choosePane };
 })();
 
 const displayController = (() => {
+  const boardElement = document.getElementById("game-board");
+  const panes = boardElement.querySelectorAll(".pane");
+
+  panes.forEach(pane => {
+    pane.addEventListener("click", () => {
+      gameBoardModule.choosePane(pane.id);
+    });
+  });
+
   const displayChoices = gameBoard => {
     for (let i = 0; i < 9; i++) {
       let pane = document.getElementById(`pane-${i + 1}`);
-      let choice = document.createElement("span");
-      choice.textContent = gameBoard[i];
-      pane.appendChild(choice);
+      pane.textContent = gameBoard[i];
     }
   };
 
   return { displayChoices };
 })();
 
-displayController.displayChoices(gameBoardModule.gameBoard);
+const player = symbol => {
+  return { symbol };
+};
 
-const player = () => {};
+const gameFlow = (() => {
+  const player1 = player("X");
+  const player2 = player("O");
+})();
+
+displayController.displayChoices(gameBoardModule.gameBoard);
